e36a9d4e3fd4de0f091cf7ed2a5df7bb941a7ffc
FruitDragon1a <fruit.dragonfruit@gmail.com>
2023-09-30 12:53:40 -0700

    Finished my part
    Read the text and separated based on word frequency

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+file = open("long.txt","r")
+text = file.read()
 
+words = text.split(" ")
+frequency = {}
+
+
+
+for i in words:
+    x = i
+    x = x.lower()
+    if x in frequency.keys():
+        frequency[x] = frequency[x]+1
+    else:
+        frequency[x] = 1
+
+print(frequency)
