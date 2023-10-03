10adb7fac1ce0dc2998cb36f375d79d4fdb496fb
atharvgups <atharvgups@gmail.com>
2023-10-02 18:03:33 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 amount = 0
 mostfrequentword = " "
-# loop through every word in dictionary to find most frequent word
-# key > amount:
-# amount = key
-# most frequent word = word
+
+for word in spWordsList:
+    if word in wordsDict:
+        wordsDict[word] += 1
+    else: 
+        wordsDict[word] = 1
+
+maxValue=1
+wWord = ""
+
+for word in wordsDict:
+    if wordsDict[word] > maxValue:
+        maxValue = wordsDict[word]
+        wWord = word
+
+print("\n\nThe word that is seen the most: " + Wword + "\nThe number of times it occured: " + str(maxValue) + "\n")
